2ea5b5d3f614479ed967bfd8be8c43141799f8f8
Andrew <milichab@gmail.com>
2019-01-27 17:16:11 -0800

    basic files

---
+++ Phind/Phind/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Phind
+//
+//  Created by Andrew B. Milich on 1/26/19.
+//  Copyright © 2019 Team-7. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Phind/Phind/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Phind
+//
+//  Created by Andrew B. Milich on 1/26/19.
+//  Copyright © 2019 Team-7. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
