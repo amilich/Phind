1a9cfa0ab45c640172a96fbb4dbeaf0005362450
Kevin Chang <kevin@kchang.me>
2019-02-09 15:00:37 -0800

    Make map view dynamic in timeline controller.

--- Phind/Phind/Controllers/TimelineController.swift
+++ Phind/Phind/Controllers/TimelineController.swift
@@ -9,14 +9,30 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import MapKit
+import RealmSwift
 
 class TimelineController: UIViewController {
+  // Constants.
+  let MAP_SPAN_LAT = 10000
+  let MAP_SPAN_LONG = 10000
+  
   // Setup all the links to the UI.
   @IBOutlet weak var currentDateLabel: UILabel!
+  @IBOutlet weak var mapView: MKMapView!
   
+  // TODO: Should this be moved into a function?
+  let realm = try! Realm()
+  
+  // viewWillAppear and viewDidLoad all follow the cycle delineated
+  // here: https://apple.co/2DqFnH6
   override func viewWillAppear(_ animated: Bool) {
     
     // Setup all the UI elements to the proper dynamic values.
+    
+    // Update current date label at the top of the screen.
+    // TODO(kevin): Update this to display date as Feb 9, 2019,
+    //              instead of Feb 09, 2019.
     let date = Date()
     let formatter = DateFormatter()
     formatter.dateFormat = "MMM dd, yyyy"
@@ -27,17 +43,21 @@ class TimelineController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    //    // Do any additional setup after loading the view, typically from a nib.
-    //    let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-    //    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-    //    view = mapView
-    //
-    //    // Creates a marker in the center of the map.
-    //    let marker = GMSMarker()
-    //    marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-    //    marker.title = "Sydney"
-    //    marker.snippet = "Australia"
-    //    marker.map = mapView
+    // Update map view to center around user's current location.
+    let userLocations = realm.objects(RealmLocation.self);
+    if (userLocations.count > 0) {
+      let userLocation = userLocations.last!
+      print(userLocation.latitude)
+      print(userLocation.longitude)
+      
+      let coordinateRegion = MKCoordinateRegion.init(
+        center: CLLocationCoordinate2D(
+          latitude: CLLocationDegrees(userLocation.latitude),
+          longitude: CLLocationDegrees(userLocation.longitude)),
+        latitudinalMeters: MAP_SPAN_LAT,
+        longitudinalMeters: MAP_SPAN_LONG)
+      mapView.setRegion(coordinateRegion, animated: true)
+    }
     
   }
   
