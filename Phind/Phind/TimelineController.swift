99da06f1aa023d255af8559fe459657cf03ad6c5
Kevin Chang <kevin@kchang.me>
2019-02-09 12:51:06 -0800

    First commit for timeline-ui.

---
+++ Phind/Phind/TimelineController.swift
@@ -0,0 +1,35 @@
+//
+//  TimelineController.swift
+//  Phind
+//
+//  Created by Andrew B. Milich on 1/26/19.
+//  Copyright © 2019 Team-7. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class TimelineController: UIViewController {
+  
+  override func viewDidLoad() {
+    
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+    view = mapView
+    
+    // Creates a marker in the center of the map.
+    let marker = GMSMarker()
+    marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+    marker.title = "Sydney"
+    marker.snippet = "Australia"
+    marker.map = mapView
+    
+  }
+  
+  
+}
+
