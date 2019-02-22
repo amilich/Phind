01839610c46cfb8ea9a951d89f4657279fde2d9c
Andrew <milichab@gmail.com>
2019-02-22 09:56:28 -0800

    adding grid and flow layout

--- Phind/Phind/Controllers/PlacePopupViewController.swift
+++ Phind/Phind/Controllers/PlacePopupViewController.swift
@@ -12,13 +12,15 @@ import RealmSwift
 import GoogleMaps
 import GooglePlaces
 
-class PlacePopupViewController: UIViewController {
+class PlacePopupViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
   
   public var place = Place()
   let label = UILabel()
   let addressLabel = UILabel()
   let backButton = UIButton()
-  let imageView = UIImageView()
+  let flowLayout = UICollectionViewFlowLayout()
+  var placeImages: [UIImage] = []
+  // let imageView = UIImageView()
   
   // Initialize the button and text elements inside the
   // place popup view.
@@ -35,12 +37,20 @@ class PlacePopupViewController: UIViewController {
     addressLabel.textAlignment = .center
     addressLabel.font = label.font.withSize(15)
     
-    imageView.frame = CGRect(x: 0, y: 150, width: UIScreen.main.bounds.width, height: 100)
-    imageView.contentMode = .scaleAspectFit
+    // imageView.frame = CGRect(x: 0, y: 150, width: UIScreen.main.bounds.width, height: 100)
+    // imageView.contentMode = .scaleAspectFit
 
     // websiteLabel.frame = CGRect(x: 0, y: 80, width: UIScreen.main.bounds.width, height: 80)
     // websiteLabel.textAlignment = .center
     // websiteLabel.font = label.font.withSize(15)
+    
+    flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 20, bottom: 5, right: 20)
+    flowLayout.itemSize = CGSize(width: 90, height: 90)
+    
+    let photoCollection = UICollectionView(frame: CGRect(x: 0, y: 130, width: self.view.frame.width, height: 250), collectionViewLayout: flowLayout)
+    photoCollection.dataSource = self
+    photoCollection.delegate = self
+    photoCollection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "PhotoCell")
     
     backButton.frame = CGRect(x: 10, y: 15, width: 50, height: 50)
     backButton.setImage(UIImage(named: "back.png"), for: .normal)
@@ -50,7 +60,7 @@ class PlacePopupViewController: UIViewController {
     self.view.addSubview(label)
     self.view.addSubview(addressLabel)
     self.view.addSubview(backButton)
-    self.view.addSubview(imageView)
+    self.view.addSubview(photoCollection)
     self.view.backgroundColor = .white
     self.view.isHidden = true
   }
@@ -65,7 +75,7 @@ class PlacePopupViewController: UIViewController {
     self.label.text = self.place.name
     self.addressLabel.text = self.place.address
     // Get rid of the previous image
-    self.imageView.image = nil
+    // self.imageView.image = nil // TODO set all images to NIL
     // Now load a new image
     self.loadPhotoForPlaceID(gms_id: place.gms_id)
   }
@@ -78,17 +88,58 @@ class PlacePopupViewController: UIViewController {
         print("Error: \(error.localizedDescription)")
         print(error)
       } else {
-        if let metadata = photoMetadata?.results.first {
-          GMSPlacesClient.shared().loadPlacePhoto(metadata, callback: { (photo, error) -> Void in
-            if let error = error {
-              print("Error: \(error.localizedDescription)")
-              print(error)
-            } else {
-              self.imageView.image = photo;
-            }
-          })
+        if photoMetadata?.results != nil {
+          self.placeImages.removeAll()
+          let numImages = (photoMetadata?.results)!.count
+          let numInGrid = min(numImages, 9)
+          if numInGrid < 3 {
+            self.flowLayout.itemSize = CGSize(width: 200, height: 200)
+          } else if numInGrid < 6 {
+            self.flowLayout.itemSize = CGSize(width: 120, height: 120)
+          } else {
+            self.flowLayout.itemSize = CGSize(width: 90, height: 90)
+          }
+          print(numImages)
+          print(numInGrid)
+          print(self.flowLayout.itemSize)
+          
+          for metadata in (photoMetadata?.results)! {
+            GMSPlacesClient.shared().loadPlacePhoto(metadata, callback: { (photo, error) -> Void in
+              if let error = error {
+                print("Error: \(error.localizedDescription)")
+                print(error)
+              } else {
+                print("Setting photo!")
+                if photo != nil {
+                  self.placeImages.append(photo!)
+                }
+                // self.imageView.image = photo;
+              }
+            })
+          }
         }
       }
     }
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let photoCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+    print("r,c = \(indexPath.row),\(indexPath.section)")
+    photoCell.backgroundColor = .red
+    return photoCell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // TODO scale photo when tapped
+    print("TODO scale photo to screen width")
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
   }
 }
