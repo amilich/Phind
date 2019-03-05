f9fc28ea3adf40c61a4442e7cfea32bc76cef993
Andrew <milichab@gmail.com>
2019-03-04 19:31:46 -0800

    adding edit view controller with search bar

---
+++ Phind/Phind/Controllers/EditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditViewController.swift
+//  Phind
+//
+//  Created by Andrew B. Milich on 3/4/19.
+//  Copyright © 2019 Team-7. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController : UIViewController {
+  
+  @IBOutlet var shadowWrap: UIView!
+  @IBOutlet var searchWrap: UIView!
+  @IBOutlet var searchBar: UISearchBar!
+}
