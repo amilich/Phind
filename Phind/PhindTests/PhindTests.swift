82cf706530cff37a8394c1e90192c0f2a993ef3e
Andrew <milichab@gmail.com>
2019-03-12 16:18:59 -0700

    adding two unit tests

--- Phind/PhindTests/PhindTests.swift
+++ Phind/PhindTests/PhindTests.swift
@@ -11,24 +11,41 @@ import XCTest
 
 class PhindTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  // Maximum time difference in seconds to allow for conversion errors
+  private static let MAX_TIME_EPS = 1.0
+  /// Test conersion from UTC to local time
+  func testLocalTimeConversion() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    let curTime = Date()
+    let locTime = Util.UTCToLocal(date: curTime)
+    let diff = locTime.timeIntervalSinceReferenceDate - curTime.timeIntervalSinceReferenceDate
+    let correctDiff = TimeInterval(TimeZone.current.secondsFromGMT(for: curTime))
+    XCTAssert(abs(correctDiff - diff) < PhindTests.MAX_TIME_EPS)
+  }
+  
+  /// Test whether utility function for determining if date is today is correct; just tests current time. TODO: Add max/min bounds for day.
+  func testIsDateToday() {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    formatter.timeZone = TimeZone.current
+    let localTime = formatter.date(from: "2019-03-09T12:00:00")!
+    XCTAssert(Util.IsDateToday(date: localTime))
+  }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+        // Put the code you want to measure the time of here.
     }
+  }
 
 }
